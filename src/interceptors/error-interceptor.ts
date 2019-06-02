5cfcd2a2ae0cbb4622efbb0d82927c996ee81d7e
brunofunk@gmail.com <brunofunk@gmail.com>
2019-06-01 22:05:31 -0300

    tratamento padrão para outros erros com alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,12 +2,14 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {}
+    constructor(public storage: StorageService,
+        public alertController: AlertController) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {        
         return next.handle(req)
@@ -24,9 +26,14 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch (errorObj.status) {
+                case 401:
+                    this.handle401();
                 case 403: 
                     this.handle403();
                     break;
+                
+                    default:
+                    this.handleDefaultError(errorObj);
             }
             return Observable.throw(errorObj);
         }) as any;
@@ -35,6 +42,36 @@ export class ErrorInterceptor implements HttpInterceptor {
     handle403() {
         this.storage.setLocalUser(null);
     }
+
+    handle401() {
+        let alerta = this.alertController.create({
+            title: 'Erro 401: falha na autenticação',
+            message: 'Email ou senha incorretos',
+            // obriga usuário a clicar no botão do alerta para fechar
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'OK'
+                }
+            ]
+        });
+        // apresenta o alerta
+        alerta.present();
+     }
+
+     handleDefaultError(errorObj) {
+        let alerta = this.alertController.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            // obriga usuário a clicar no botão do alerta para fechar
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'OK'
+                }
+            ]
+        });
+        // apresenta o alerta
+        alerta.present();
+     }
 }
 
 export const ErrorInterceptorProvider = {
